9be678c19e41cbe447247f960034186626b3860a
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-09-18 11:29:59 -0300

    feat: add support for php development

--- init.el
+++ init.el
@@ -147,8 +147,9 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       org                 ; organize your plain life in plain text
+       (php
+        +lsp)              ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
