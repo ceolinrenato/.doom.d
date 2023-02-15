50d83b98aa6c63535df9530a2513835a175ff63d
Renato Ceolin <renato.ceolin@renatoceolin.com>
2023-02-15 13:39:54 -0300

    Temporary fix for magit updates breaking for Emacs 28.2
    Related to issue: https://github.com/doomemacs/doomemacs/issues/7078

--- packages.el
+++ packages.el
@@ -50,3 +50,11 @@
 ;(unpin! t)
 (package! protobuf-mode)
 (package! graphql-mode)
+
+(package! transient
+      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+      :recipe (:host github :repo "magit/transient"))
+
+(package! with-editor
+          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+          :recipe (:host github :repo "magit/with-editor"))
