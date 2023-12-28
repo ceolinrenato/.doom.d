7d9aa2cc650b9c7525463731ed3dd9837061fc48
Renato Ceolin <renato.ceolin@thescore.com>
2023-12-28 17:03:13 -0300

    adds rust support

--- init.el
+++ init.el
@@ -115,8 +115,7 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       (cc
-	 +lsp); C/C++/Obj-C madness
+       (cc +lsp); C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -140,8 +139,7 @@
        ;;idris             ;
        json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       (javascript
-         +lsp)             ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)             ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
@@ -169,7 +167,7 @@
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        (ruby +rails +lsp)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
